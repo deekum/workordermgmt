7a10cc4a5988875fb50337c5113d259085ec2d5a
deekum <deepak.kumar@evry.com>
2020-10-14 17:41:49 +0530

    Created Servicer model

--- src/models/servicer.js
+++ src/models/servicer.js
@@ -1,1 +1,18 @@
-//servicer model
+const mongoose = require('mongoose')
+
+const servicerSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        tokens: [{
+            token: {
+                type: String
+            }
+        }]
+    }
+)
+
+const servicer = mongoose.model('Servicer', servicerSchema)
+module.exports = servicer
