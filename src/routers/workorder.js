b8f0088bf1722d4b9785380cc105ac66da25d204
Deepak Kumar <Deepak.Kumar@evry.com>
2020-11-10 12:18:53 +0530

    Add route for getworkorders and getallworkorders

--- src/routers/workorder.js
+++ src/routers/workorder.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const Workorder = require('../models/workorder')
-const { ObjectID } = require('mongodb')
 const Company = require('../models/company')
-const Technician = require('../models/technician')
 const jwt = require('jsonwebtoken')
 const {logger, serviceLogger} = require('../logger/logger')
 
@@ -17,14 +15,13 @@ router.post('/createworkorder', async (request, response)=>{
             logger.error('Not able to identify company')
             throw new Error('Not able to identify company')
         }
-        logger.info('create order')
         const workorder = new Workorder(request.body)
         workorder.status = 'created'
         workorder.company = company._id 
         await workorder.save()
         response.status(201).send(workorder)
     } catch(error){
-        //console.log(error)
+        console.log(error)
         response.status(400).send(error)
     }
 
@@ -47,7 +44,7 @@ router.post('/updateworkorder', async (request, response)=>{
             return allowedUpdates.includes(item)
         })
 
-        console.log(isValidOperation)
+        logger.debug('is Valid for input for updateworkorder:',isValidOperation)
         if(!isValidOperation){
             logger.error('Invalid request to update workorder status')
             return response.status(404).send('Invalid request to update workorder status')
@@ -58,14 +55,67 @@ router.post('/updateworkorder', async (request, response)=>{
             return response.status(404).send("No workorder found")
         }   
         workorder.status = request.body.status
-        
         await workorder.save()
             
         response.status(200).send(workorder)
         
     } catch(error){
-        //console.log(error)
+        console.log(error)
         logger.error('Error while update workorder ',error)
+        response.status(500).send(error)
+    }
+
+})
+
+router.get("/company/getworkorders/:workorderid?", async (request, response)=>{
+    const workorderid = request.params.workorderid
+    try{
+        const token = request.header('Authorization').replace('Bearer ','')
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+        
+        const company = await Company.findOne({_id: decode._id})
+        if(!company){
+            logger.error('getworkorders is not requested by a valid company')
+            throw new Error('Not able to identify company')
+        } 
+
+        let workorders
+        if(!workorderid){
+            workorders = await Workorder.find({company}).select('-company -_id -__v')   
+        } else {
+            workorders = await Workorder.findOne({company,workorderid}).select('-company -_id -__v')  
+        }
+        console.log(workorders)
+        if(!workorders){
+            logger.error('No workorder found')
+            return response.status(404).send("No workorder found")
+        }   
+        response.status(200).send(workorders)
+        
+    } catch(error){
+        console.log(error)
+        logger.error('Error while fetching workorders ',error)
+        response.status(500).send(error)
+    }
+
+})
+
+router.get("/getallworkorders", async (request, response)=>{
+    try{
+        const workorders = await Workorder.find().select('-_id -__v').populate({
+            path:'company',
+            select:'name -_id'
+        })   
+        
+        if(!workorders){
+            logger.error('No workorder found')
+            return response.status(404).send("No workorder found")
+        }   
+        response.status(200).send(workorders)
+        
+    } catch(error){
+        console.log(error)
+        logger.error('Error while fetching all workorders ',error)
         response.status(500).send(error)
     }
 
