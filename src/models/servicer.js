80b258f2575f31d66cd294596d243af0bb199a72
deekum <deepak.kumar@evry.com>
2020-11-17 22:19:28 +0530

    Updated Servicer model (Chethana)

--- src/models/servicer.js
+++ src/models/servicer.js
@@ -1,10 +1,31 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const servicerIdAutoIncrement = require('mongoose-auto-increment')
+const jwt = require('jsonwebtoken')
 
 const servicerSchema = new mongoose.Schema(
     {
-        name: {
+        servicerid: {
+            type: Number,
+            required: true
+        },
+        servicerName: {
             type: String,
             required: true
+        },
+        servicerAddress: {
+            type: String,
+            required: true
+        },
+        servicerPhone: {
+            type: String,
+            validate:{
+                validator: function(v){
+                    return /\d{10}/.test(v);
+                },
+                message: props => '${props.value} is not a valid phone number'
+            },
+            required:[true,'Servicer phone is required']
         },
         tokens: [{
             token: {
@@ -13,6 +34,21 @@ const servicerSchema = new mongoose.Schema(
         }]
     }
 )
+
+servicerIdAutoIncrement.initialize(mongoose.connection)
+servicerSchema.plugin(servicerIdAutoIncrement.plugin, {
+    model:'servicer',
+    field: 'servicerid',
+    startAt: 1,
+    incrementBy: 1
+})
+
+servicerSchema.methods.generateAuthToken = async function () {
+    const servicer = this
+    const token = jwt.sign({ _id: servicer._id.toString()}, process.env.JWT_SECRET)
+    servicer.tokens = servicer.tokens.concat({ token })
+    return token
+}
 
 const servicer = mongoose.model('Servicer', servicerSchema)
 module.exports = servicer
