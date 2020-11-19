f6f340ad9b2f502407b657067bffb989558b69b3
deekum <deepak.kumar@evry.com>
2020-11-19 11:12:22 +0530

    Updated Servicer router (Chethana)

--- src/routers/servicer.js
+++ src/routers/servicer.js
@@ -11,7 +11,8 @@ servicerRouter.post('/createServicer', async (req, res) => {
         await servicerObject.save()      
         res.status(201).send({servicerObject, token})
     } catch (error) {
-        res.status(400).send(error)
+        logger.error('Error while creating the Servicer: ', error)
+        res.status(500).send(error)
     }
 })
 
@@ -20,12 +21,14 @@ servicerRouter.get('/getServicer', async (req, res) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
         const decode = jwt.verify(token, process.env.JWT_SECRET)
-        const servicers = await Servicer.findOne({ _id: decode._id, 'tokens.token': token})
-        if(!servicers){
-            return res.status(404).send(servicers)
+        const servicers = await Servicer.findOne({ _id: decode._id, 'tokens.token': token })
+        if (!servicers) {
+            logger.error('Servicer not found!')
+            return res.status(404).send({ error: 'Servicer not found!' })
         }
-        res.send(servicers)                    
-    } catch(error) {
+        res.send(servicers)
+    } catch (error) {
+        logger.error('Error getting the Servicer: ', error)
         res.status(500).send(error)
     }
 })
