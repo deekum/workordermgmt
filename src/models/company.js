66a34d8702fb634efe1807fc3ed0eaeaa7fb3c6f
deekum <deepak.kumar@evry.com>
2020-11-19 13:37:25 +0530

    Uploaded Company related changes - Nirupama

--- src/models/company.js
+++ src/models/company.js
@@ -1,11 +1,32 @@
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
+const idAutoIncrement = require('mongoose-auto-increment')
+
 const companySchema = new mongoose.Schema(
     {
+        companyId: {
+            type: Number,
+            required: true
+        },
         name: {
             type: String,
-            required: true
+            required:[true, 'Company Name is required'],
+            unique:true            
         }, 
+        companyAddress: {
+            type: String,
+            required:[true, 'Company Address is required'],
+        },
+        companyPhone: {
+            type: String,
+            validate:{
+                validator: function(v){
+                    return /\d{10}/.test(v);
+                },
+                message: props => '${props.value} is not a valid phone number'
+            },
+            required:[true,'Company phone is required']
+        },
         token : {
             type: String,
             //required: true
@@ -22,6 +43,14 @@ companySchema.pre('save', async function(next){
     next()
 })
 
+
+idAutoIncrement.initialize(mongoose.connection)
+companySchema.plugin(idAutoIncrement.plugin, {
+    model:'company',
+    field: 'companyId',
+    startAt: 1,
+    incrementBy: 1
+})
 
 const company = mongoose.model('Company', companySchema)
 module.exports = company
